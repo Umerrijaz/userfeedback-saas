150dc49a57c8c6dff704b5dd2b42e9567071d19c
Umerrijaz <umerijaaz@protonmail.com>
2026-03-31 15:52:48 +0200

    updated here section

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn btn-primary">Button</button>
+      <h1>Kundenfeedback erfassen, um bessere Produkte zu entwickeln</h1>
+      <div>
+        Erstelle in wenigen Minuten ein Feedback-Board, priorisiere Funktionen
+        und entwickle Produkte, die deine Kunden lieben werden
+      </div>
     </main>
   );
 }
